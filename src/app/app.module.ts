377f1486c6d7b326fb3f4914818c3ad8cd76415a
deverma32 <deverma@1234>
2019-06-29 17:10:51 +0530

    ChatbotAi

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatModule } from './chat/chat.module';
+import { ChatDialogComponent } from './chat/chat-dialog/chat-dialog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ChatModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
